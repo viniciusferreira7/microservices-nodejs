008f89b9d5ce7b2fa8d9108ea7e9bd13a8e04e7b
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-02 10:08:41 -0300

    feat: add timeout and max retries on api gateway

--- app-orders/src/tracer/tracer.ts
+++ app-orders/src/tracer/tracer.ts
@@ -1,6 +1,4 @@
-import { trace } from '@opentelemetry/api';
-import { env } from '../../env.ts';
+import { trace } from "@opentelemetry/api";
+import { env } from "../../env.ts";
 
 export const tracer = trace.getTracer(env.OTEL_SERVICE_NAME);
-
-// TODO: 1:23:10
