a4d55bdd0a821dd612cff4797e8ab2720339656b
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-06 10:24:18 -0300

    feat: create ecr repository

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,30 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as docker from "@pulumi/docker-build";
+import * as pulumi from "@pulumi/pulumi";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("my-bucket");
+const ordersECRRepository = new awsx.ecr.Repository("orders-ecr", {
+	forceDelete: true,
+});
 
-// Export the name of the bucket
-export const bucketName = bucket.id;
+const ordersECRToken = aws.ecr.getAuthorizationTokenOutput({
+	registryId: ordersECRRepository.repository.registryId,
+});
+
+export const ordersDockerImage = new docker.Image("orders-image", {
+	tags: [
+		pulumi.interpolate`${ordersECRRepository.repository.repositoryUrl}:latest`,
+	],
+	context: {
+		location: "../app-orders",
+	},
+	push: true,
+	platforms: ["linux/amd64"],
+	registries: [
+		{
+			address: ordersECRRepository.repository.repositoryUrl,
+			username: ordersECRToken.userName,
+			password: ordersECRToken.password,
+		},
+	],
+});
