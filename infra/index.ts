aa8fc5dffc81807e82d9354a91f43d78b100a81a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-20 10:04:54 -0300

    feat: create listener to orders service

--- infra/index.ts
+++ infra/index.ts
@@ -6,5 +6,3 @@ import { rabbitMQService } from "./src/services/rabbitmq";
 export const ordersId = ordersService.service.id;
 export const rabbitMQId = rabbitMQService.service.id;
 export const rabbitMQAdminUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
-
-//TODO: 28:00
--- infra/src/services/orders.ts
+++ infra/src/services/orders.ts
@@ -2,7 +2,29 @@ import * as awsx from "@pulumi/awsx";
 import * as pulumi from "@pulumi/pulumi";
 import { cluster } from "../cluster";
 import { ordersDockerImage } from "../images/orders";
+import { appLoadBalancer } from "./load-balancer";
 import { amqpListener } from "./rabbitmq";
+
+export const ordersTargetGroup = appLoadBalancer.createTargetGroup(
+	"orders-admin-target",
+	{
+		port: 3333,
+		protocol: "HTTP",
+		healthCheck: {
+			path: "/health",
+			protocol: "HTTP",
+		},
+	},
+);
+
+export const ordersAdminHttpListener = appLoadBalancer.createListener(
+	"orders-admin-listener",
+	{
+		port: 3333,
+		protocol: "HTTP",
+		targetGroup: ordersTargetGroup,
+	},
+);
 
 export const ordersService = new awsx.classic.ecs.FargateService(
 	"fargate-orders",
@@ -15,6 +37,7 @@ export const ordersService = new awsx.classic.ecs.FargateService(
 				image: ordersDockerImage.ref,
 				cpu: 256,
 				memory: 512,
+				portMappings: [ordersAdminHttpListener],
 				environment: [
 					{
 						name: "BROKER_URL",
@@ -24,8 +47,20 @@ export const ordersService = new awsx.classic.ecs.FargateService(
 						name: "DATABASE_URL",
 						value: "", //FIXME: Create database using neon, pulumi doesn`t have native config to create it on neon
 					},
+					{ name: "OTEL_TRACES_EXPORTER", value: "otlp" }, //FIXME: It`s necessary create grafana project
+					{
+						name: "OTEL_EXPORTER_OTLP_ENDPOINT",
+						value: "http://localhost:4318",
+					}, //FIXME: It`s necessary create grafana project
+					{ name: "OTEL_SERVICE_NAME", value: "orders" }, //FIXME: It`s necessary create grafana project
+					{
+						name: "OTEL_NODE_ENABLED_INSTRUMENTATIONS",
+						value: "http,fastify,pg,amqplib",
+					}, //FIXME: It`s necessary create grafana project
 				],
 			},
 		},
 	},
 );
+
+//TODO: 53:38
