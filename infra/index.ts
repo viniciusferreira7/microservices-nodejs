1668aa3fb01aa75bfd40ae746096e297599e5009
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-10-23 10:49:59 -0300

    feat: expose kong id

--- infra/index.ts
+++ infra/index.ts
@@ -1,10 +1,15 @@
 import * as pulumi from "@pulumi/pulumi";
 import { invoicesService } from "./src/services/invoices";
+import { kongService } from "./src/services/kong";
 import { appLoadBalancer } from "./src/services/load-balancer";
 import { ordersService } from "./src/services/orders";
 import { rabbitMQService } from "./src/services/rabbitmq";
 
 export const ordersId = ordersService.service.id;
+
 export const invoicesId = invoicesService.service.id;
+
 export const rabbitMQId = rabbitMQService.service.id;
 export const rabbitMQAdminUrl = pulumi.interpolate`http://${appLoadBalancer.listeners[0].endpoint.hostname}:15672`;
+
+export const kongId = kongService.service.id;
